2948392592ee03f38a82b1a40b85818df07887fd
tochman <thomas@nocebo.se>
2015-05-29 14:43:56 +0200

    getting to know the code

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -1,13 +1,14 @@
 class Charity < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_attached_file :logo,
     :styles => { :thumb => "200x200" },
     :default_url => "/images/oodls-placeholder.svg"
+
 	validates_attachment_content_type :logo, :content_type => /\Aimage\/.*\Z/
+
   validates_presence_of :organisation, :postcode, :full_address
 
   geocoded_by :address
