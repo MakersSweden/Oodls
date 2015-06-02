d54496cfb621bed9508c3a8bc890c1458b85b03f
tochman <thomas@nocebo.se>
2015-06-02 17:08:07 +0200

    added display of donations on donors#show

--- app/helpers/donors_helper.rb
+++ app/helpers/donors_helper.rb
@@ -1,2 +1,20 @@
 module DonorsHelper
+  def claimed_by(donation)
+    claims = donation.donation_claims.each.map {|claim| claim.charity.organisation}
+    claims.join(' ')
+  end
+
+  def delivered_to(donation)
+    if accepted_charity(donation)
+      donation.donation_claims.where(accepted: true).map {|d|d.charity.organisation}
+    elsif donation.donation_claims.where(accepted: [false, nil]).any?
+      'not accepted'
+    end
+  end
+
+  private
+  def accepted_charity(donation)
+    donation.donation_claims.where(accepted: true).any? ? true : false
+  end
+
 end
