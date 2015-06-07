4e536bb7962513119eba13fc1dba213ffe34dc45
tochman <thomas@nocebo.se>
2015-06-07 21:51:45 +0200

    added error message to eniro controller response

--- app/controllers/eniro_validation_controller.rb
+++ app/controllers/eniro_validation_controller.rb
@@ -1,10 +1,12 @@
 class EniroValidationController < ApplicationController
-
+ # Usage [host].eniro_validation?query=[query]&ccode=[country code se, no, dk]
   def eniro_validation
     query = params[:query]
     ccode = params[:ccode]
     results = EniroApi.new(query, ccode).query.sanitize_api_response.to_json
     render json: results
+  rescue
+    render json: {error: 'wrong format', message: 'EniroValidation API does not recognize the request'}
   end
 
 end
