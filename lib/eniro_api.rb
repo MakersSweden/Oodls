888cb9bb1502f2425418778140a34825fe54cee8
tochman <thomas@nocebo.se>
2015-06-07 10:11:26 +0200

    refactored API response

--- lib/eniro_api.rb
+++ lib/eniro_api.rb
@@ -1,5 +1,8 @@
 class EniroApi
-  #http://api.eniro.com/partnerapi/cs/search/basic?profile=Oodls.io&key=1325023846608000432&country=se&version=1.1.3&search_word=Thomas&Ochman
+  # Usage:
+  # EniroApi.new('[company name]', 'se').query.sanitize_api_response
+  # Works with 'se', 'no' & 'dk'
+
   BASE_URL = 'http://api.eniro.com/partnerapi/cs/'
   PROFILE = 'Oodls.io'
   API_KEY = '1325023846608000432'
@@ -24,14 +27,31 @@ class EniroApi
         search_word: @query
     }.to_query
   end
+
 end
 
 class Array
-  def names
+  def sanitize_api_response
     results_names = []
-    self.each do |result|
-      results_names << result['companyInfo']['companyName']
+    self.each do |r|
+      results_names << {companyName: r['companyInfo']['companyName'],
+                        orgNumber: r['companyInfo']['orgNumber'],
+                        eniroId: r['eniroId'],
+                        streetName: r['address']['streetName'],
+                        postCode: r['address']['postCode'],
+                        city: r['address']['postArea'],
+                        website: get_url(r['homepage']),
+                        latitude: r['location']['coordinates'][0]['latitude'].to_f,
+                        longitude: r['location']['coordinates'][0]['longitude'].to_f}
+
     end
     results_names
+  end
+
+  def get_url(url)
+    response = open(url).read
+    response.match(/meta http-equiv="REFRESH" content="0;url=(.+)">/)[1]
+  rescue
+    'nil'
   end
 end
