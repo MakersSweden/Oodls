59078bb61059bf9e07758d16925d9648cc868a6a
Sampriti Panda <sampdingo@hotmail.com>
2015-06-09 13:51:40 +0530

    Country Code from ip address

--- app/controllers/eniro_validation_controller.rb
+++ app/controllers/eniro_validation_controller.rb
@@ -2,7 +2,11 @@ class EniroValidationController < ApplicationController
  # Usage [host].eniro_validation?query=[query]&ccode=[country code se, no, dk]
   def eniro_validation
     query = params[:term]
-    ccode = 'se'
+    begin
+      ccode = Geocoder.search(request.env['HTTP_X_REAL_IP'])[0].country_code.downcase
+    rescue
+      ccode = 'se'
+    end
     results = EniroApi.new(query, ccode).query.sanitize_api_response.to_json
     render json: results
   rescue
